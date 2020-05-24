a413b1c9c93041e9c053e1817b52f13aa69afcf6
andreylobach <andrey.lobach@mail.ru>
2020-05-24 10:53:39 +0300

    Run service

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from service import app
 from service import host
 from service import port
 
-app.run(host=host, port=port, debug=True, threaded=True) 
+app.run(host=host, port=port, debug=True, threaded=True)  
